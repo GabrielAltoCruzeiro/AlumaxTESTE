d4ef8e56e288e53e69ec75f95a5f7fe94e80eae5
Gabriel <Gabrielalto308@gmail.com>
2026-06-28 17:26:23 -0300

    usa API brevo para envio de email

--- home/views.py
+++ home/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
-from django.core.mail import send_mail
+from django.http import HttpResponse
+import requests
+import os
 
 def home(request):
-   
-    
     if request.method == 'POST':
         nome = request.POST.get('nomeUsuario')
         email = request.POST.get('emailUsuario')
@@ -14,49 +13,31 @@ def home(request):
         detalhe = request.POST.get('detalheUsuario')
 
         if not nome:
-             return HttpResponse("Nome é obrigatório Por favor retone e adicione seu nome")
-
+            return HttpResponse("Nome é obrigatório. Por favor retorne e adicione seu nome.")
         if not email:
             return HttpResponse("Email é obrigatório, por favor retorne e adicione.")
-
         if not setor:
             return HttpResponse("Setor é obrigatório, retorne e adicione.")
         if not suporte:
-            return HttpResponse("Você precisa adicionar o tipo de suporte")
-        
-        
-        
-        
+            return HttpResponse("Você precisa adicionar o tipo de suporte.")
 
-        send_mail(
-                 subject="Novo Chamado de TI",
+        url = "https://api.brevo.com/v3/smtp/email"
+        headers = {
+            "accept": "application/json",
+            "api-key": os.environ.get('BREVO_API_KEY'),
+            "content-type": "application/json"
+        }
+        payload = {
+            "sender": {"name": "Alumax TI", "email": "b030cd001@smtp-brevo.com"},
+            "to": [
+                {"email": "gabriel.mendonca@alumax.ind.br"},
+                {"email": "gabrielalto308viol@gmail.com"}
+            ],
+            "subject": "Novo Chamado de TI",
+            "textContent": f"Nome: {nome}\nEmail: {email}\nSetor: {setor}\nSuporte: {suporte}\nCategoria: {categoria}\nDetalhes: {detalhe}"
+        }
+        requests.post(url, json=payload, headers=headers)
 
-                 message=f"""
-                 Nome: {nome},
-                 Email: {email},
-                 Setor:{setor}, 
-                 Suporte:{suporte},
-                 Categoria:{categoria},
-                 Detalhes:{detalhe}, 
-                 
-
-
-                 """,
-
-    from_email="gabrielalto.tech@outlook.com",
-
-    recipient_list=["gabriel.mendonca@alumax.ind.br", " gabrielalto308viol@gmail.com"],
-
-    fail_silently=False,
-)
-      
         return render(request, 'obrigado.html')
-     
-     
+
     return render(request, 'index.html')
-    
-    
-
-    
-
-       
